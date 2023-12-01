81c1779407b4b58bf8e44d3d4d9a15566cf48a43
Shahboz <sharafzodashahboz@gmail.com>
2023-12-01 16:54:57 +0500

    pytest for fixture added

--- tests/test_ex1.py
+++ tests/test_ex1.py
@@ -1,17 +1,17 @@
-import pytest 
-from django.contrib.auth.models import User 
+# import pytest 
+# from django.contrib.auth.models import User 
 
-@pytest.mark.django_db
-def test_user_create():
-    User.objects.create_user('test','test@test.com','test')
-    count = User.objects.all().count()
-    print(count)
-    assert User.objects.all().count() == 1
+# @pytest.mark.django_db
+# def test_user_create():
+#     User.objects.create_user('test','test@test.com','test')
+#     count = User.objects.all().count()
+#     print(count)
+#     assert User.objects.all().count() == 1
 
-@pytest.mark.django_db
-def test_user_create1():
-    count = User.objects.all().count()
-    assert count == 0
+# @pytest.mark.django_db
+# def test_user_create1():
+#     count = User.objects.all().count()
+#     assert count == 0
 
 
     
